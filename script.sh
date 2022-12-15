4e96a9d52b1643cd426d280018d7b01a6dffc7f8
Sebin Benjamin <sebinbenjamin@gmail.com>
2022-12-15 16:18:48 +1300

    initial commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+docker build . -t sebinbenjamin/node-web-app
+# docker images
+# docker run -p 49160:8080 -d <your username>/node-web-app
+# docker ps
+# docker logs <container id>
+# docker exec -it <container id> /bin/bash
